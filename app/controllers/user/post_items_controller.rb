55efa6699417d7f1ebddda0dba21223c2d2f60ef
ooishiho <yuidacheng697@gmail.com>
2023-05-12 08:44:42 +0000

    [add]post_item

--- app/controllers/user/post_items_controller.rb
+++ app/controllers/user/post_items_controller.rb
@@ -1,13 +1,24 @@
 class User::PostItemsController < ApplicationController
   def index
+    @post_item = PostItem.all
   end
 
   def new
+    @post_item = PostItem.new
   end
 
   def create
+    @post_item = PostItem.new(post_item_params)
+    @post_item.save
+    redirect_to user_post_item_path(@post_item.id)
   end
 
   def show
+    @post_item = PostItem.find(params[:id])
+  end
+
+  private
+  def post_item_params
+     params.require(:post_item).permit(:introduction,:image)
   end
 end
--- app/models/post_item.rb
+++ app/models/post_item.rb
@@ -2,4 +2,14 @@ class PostItem < ApplicationRecord
   belongs_to :user
   has_many :bookmarks
   has_many :evaluations
+
+  has_one_attached :image
+  
+  def get_item_image(width,height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image_square.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+    image.variant(resize_to_limit: [width,height]).processed
+  end
 end
