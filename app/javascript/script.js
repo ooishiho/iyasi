34375dbc57ebf3d64193e8de99dbbec99bc8206d
ooishiho <yuidacheng697@gmail.com>
2023-06-20 03:05:07 +0000

    [fix]swiper

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,7 +1,19 @@
 /*global Swiper*/
 // Swiperのオプションを定数化
 const opt = {
+
+  fadeEffect: {
+    crossFade: true,
+  },
+
   loop: true,
+  loopAdditionalSlides: 1,
+  speed: 2000,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },
   pagination: {
     el: '.swiper-pagination',
   },
